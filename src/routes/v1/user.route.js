787305203fb775f926fbcacb2535f6e7bc46513c
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2023-03-14 09:04:04 +0800

    routes: update /users access

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -9,11 +9,11 @@ const router = express.Router();
 router
   .route('/')
   .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
-  .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
+  .get(auth(), validate(userValidation.getUsers), userController.getUsers);
 
 router
   .route('/:userId')
-  .get(auth(), validate(userValidation.getUser), userController.getUser)
+  .get(auth('manageUsers'), validate(userValidation.getUser), userController.getUser)
   .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
   .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
 
