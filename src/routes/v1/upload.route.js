8ded8a6bb01d4f9cb7f19cedc649c4b5734a28ed
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2023-11-05 00:34:27 +0800

    remove auth from upload

--- src/routes/v1/upload.route.js
+++ src/routes/v1/upload.route.js
@@ -4,6 +4,13 @@ const uploadController = require('../../controllers/upload.controller');
 
 const router = express.Router();
 
-router.get('/', auth(), uploadController.getPresignedUrl);
+// router.get('/', auth(), uploadController.getPresignedUrl);
+router.get('/', uploadController.getPresignedUrl);
 
 module.exports = router;
+
+/*
+  1 - CLIENT network request for presigned url
+  2 - SERVER get presigned url from AWS S3
+  3 - SERVER send back presigned url to CLIENT
+*/
