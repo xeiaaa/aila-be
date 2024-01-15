d23ae26d592b78a49e95f877da9ccb7be3c4798e
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2024-01-15 15:48:10 +0800

    Debug Auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,8 +3,11 @@ const catchAsync = require('../utils/catchAsync');
 const { authService, userService, tokenService, emailService } = require('../services');
 
 const register = catchAsync(async (req, res) => {
+  console.log('Auth Controller / register', req.body);
   const user = await userService.createUser(req.body);
+  console.log('Auth Controller / register - user', user);
   const tokens = await tokenService.generateAuthTokens(user);
+  console.log('Auth Controller / tokens', tokens);
   res.status(httpStatus.CREATED).send({ user, tokens });
 });
 
